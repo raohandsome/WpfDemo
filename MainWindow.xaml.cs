228aef6351e0f283a97b3914da7a9b2488eedb49
raohandsome <rao5like6>
2019-12-25 11:19:46 +0800

    init repo

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using UIHelper;
+
+namespace TextViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new Model();
+            this.DataContext = _Model;
+        }
+
+        private Model _Model;
+
+        private void OnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnOpen_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            OpenTextFileDialog aDlg = new OpenTextFileDialog();
+            if (aDlg.ShowDialog() != true) return;
+            try
+            {
+                _Model.Load(aDlg.FileName, aDlg.CurrentEncoding);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnStartFilte_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                _Model.StartFilte();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnStartFilte_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null && _Model.CanStartFilte;
+        }
+
+        private void OnOpenPictureFile_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog aDlg = new OpenFileDialog()
+            {
+                Title = "选择图片",
+                Filter = "图片文件 (*.jpg)|*.jpg|图片文件 (*.*)|*.*",
+            };
+            if (aDlg.ShowDialog() != true) return;
+            _Model.PictureFileName = aDlg.FileName;
+        }
+
+        private void OnOpenPictureFile_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnSendEmail_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("发送成功！");
+        }
+
+        private void OnSendEmail_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null && !string.IsNullOrEmpty(_Model.EmailAddress);
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextViewer
+{
+    class Model : INotifyPropertyChanged
+    {
+        public void Load(string aFileName, Encoding aEncoding)
+        {
+            Text = File.ReadAllText(aFileName, aEncoding);
+            if (!CanStartFilte)
+                FiltedText = Text;
+            else
+                StartFilte();
+        }
+
+        public string Text
+        {
+            get => _Text;
+            set
+            {
+                if (_Text == value) return;
+                _Text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        private string _Text;
+
+        public string FiltedText
+        {
+            get => _FiltedText;
+            set
+            {
+                if (_FiltedText == value) return;
+                _FiltedText = value;
+                OnPropertyChanged(nameof(FiltedText));
+            }
+        }
+        private string _FiltedText;
+
+        public string Pattern
+        {
+            get => _Pattern;
+            set
+            {
+                if (_Pattern == value) return;
+                _Pattern = value;
+                OnPropertyChanged(nameof(Pattern));
+            }
+        }
+        private string _Pattern;
+
+        private void OnPropertyChanged(string aPropertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void StartFilte()
+        {
+            string[] aLines = Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder aStringBuilder = new StringBuilder();
+            Regex aRegex = new Regex(Pattern);
+            foreach(var aLine in aLines)
+            {
+                if (aRegex.IsMatch(aLine))
+                {
+                    aStringBuilder.AppendLine(aLine);
+                }
+            }
+            FiltedText = aStringBuilder.ToString();
+        }
+        public bool CanStartFilte => 
+            !string.IsNullOrWhiteSpace(Text) && 
+            !string.IsNullOrWhiteSpace(Pattern);
+
+        private string _PictureFileName;
+        public string PictureFileName 
+        { 
+            get => _PictureFileName;
+            set
+            {
+                if (_PictureFileName == value) return;
+                _PictureFileName = value;
+                OnPropertyChanged(nameof(PictureFileName));
+            }
+        }
+
+        private string _EmailAddress;
+        public string EmailAddress
+        {
+            get => _EmailAddress;
+            set
+            {
+                if (_EmailAddress == value) return;
+                _EmailAddress = value;
+                OnPropertyChanged(nameof(EmailAddress));
+            }
+        }
+    }
+}
